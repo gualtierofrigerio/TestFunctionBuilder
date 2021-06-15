99d4b731c5875e085227aced8d2d0f36edd8475b
Gualtiero <gualtiero.frigerio@gmail.com>
2021-06-15 11:03:01 +0200

    added new example

--- TestFunctionBuilder/ViewController.swift
+++ TestFunctionBuilder/ViewController.swift
@@ -12,19 +12,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let statement = false
         let view1 = SimpleView(withBackgroundColor: .yellow)
         let view2 = SimpleView(withBackgroundColor: .red)
+        let view3 = SimpleView(withBackgroundColor: .green)
         let frame = self.view.frame
-        let containerView = ContainerView(frame:frame, align:.vertical) {
+//        let containerView = ContainerView(frame:frame, align:.vertical) {
+//            view1
+//            view2
+//            SimpleView(withBackgroundColor: .blue)
+//            ContainerView(frame:frame, align:.horizontal) {
+//                SimpleView(withBackgroundColor: .orange)
+//                SimpleView(withBackgroundColor: .green)
+//            }
+//        }
+        let container = ContainerView(frame: frame) {
             view1
-            view2
-            SimpleView(withBackgroundColor: .blue)
-            ContainerView(frame:frame, align:.horizontal) {
-                SimpleView(withBackgroundColor: .orange)
-                SimpleView(withBackgroundColor: .green)
+            if statement {
+                view2
+            }
+            else {
+                view3
+            }
+            if statement {
+                view2
             }
         }
-        self.view.addSubview(containerView.view)
+        //self.view.addSubview(containerView.view)
+        self.view.addSubview(container.view)
     }
 }
 
