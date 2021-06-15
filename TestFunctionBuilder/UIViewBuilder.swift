fb3330c5ae5b60b83e685fc01caa586570201680
Gualtiero <gualtiero.frigerio@gmail.com>
2021-06-15 11:02:42 +0200

    @_functionBuilder replaced by @resultBuilder

--- TestFunctionBuilder/UIViewBuilder.swift
+++ TestFunctionBuilder/UIViewBuilder.swift
@@ -11,11 +11,60 @@ import UIKit
 
 protocol UIKitView {
     var view:UIView{get}
+    var views:[UIView] {get}
 }
 
-@_functionBuilder
+extension UIKitView {
+    var views:[UIView] {
+        [view]
+    }
+}
+
+struct UIKitViewArray: UIKitView {
+    var view: UIView {
+        if storedUIKitViews.count > 0 {
+            return storedUIKitViews[0].view
+        }
+        return UIView()
+    }
+    
+    var views:[UIView] {
+        storedUIKitViews.map{$0.view}
+    }
+    
+    init(withViews views:[UIKitView]) {
+        storedUIKitViews = views
+    }
+    
+    private var storedUIKitViews:[UIKitView] = []
+}
+
+
+@resultBuilder
 struct UIViewBuilder {
     static func buildBlock(_ views:UIKitView...) -> [UIView] {
         views.map{$0.view}
+    }
+}
+
+@resultBuilder
+struct UIKitViewBuilder {
+    static func buildBlock(_ views:UIKitView...) -> UIKitView {
+        print("buildBlock views \(views)")
+        return UIKitViewArray(withViews: views)
+    }
+    
+    static func buildEither(first: UIKitView) -> UIKitView {
+        print("buildEither first \(first)")
+        return first
+    }
+
+    static func buildEither(second: UIKitView) -> UIKitView {
+        print("buildEither second \(second)")
+        return second
+    }
+    
+    static func buildIf(_ view:UIKitView?) -> UIKitView {
+        view ?? EmptyView()
     }
 }
